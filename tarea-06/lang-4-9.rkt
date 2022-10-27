a5e29f1215a21153c97ab9582fe9a41eec8fc923
Sergio López <sergioadairlopez@gmail.com>
2022-10-26 23:39:15 -0700

    Add files via upload

---
+++ tarea-06/lang-4-9.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(define (empty-store) (vector))
+
+(define the-store 'uninitialized)
+
+(define (get-store) the-store)
+
+(define (initialize-store!)
+  (set! the-store (empty-store)))
+
+(define (reference? v)
+  (and (integer? v)
+       (not (negative? v))))
+
+(define (newref val)
+  (let ((next-ref (vector-length the-store)))
+    (set! the-store (vector-append the-store (vector val)))
+    next-ref ))
+
+(define (deref ref )
+  (vector-ref the-store ref ))
+
+(define (report-invalid-reference ref the-store)
+  (error 'setref "referencia no válida ~e en almacenamiento ~e" ref the-store))
+
+(define (setref! ref val)
+  (vector-set! the-store
+               (if (and (reference? ref)
+                        (< ref (vector-length the-store)))
+                   ref
+                   (report-invalid-reference ref the-store))
+               val))
